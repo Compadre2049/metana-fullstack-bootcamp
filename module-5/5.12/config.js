a9668f29c1821e2b3b2eb79313f5e9aa2de137e0
Nathan Obenchain <nathanobenchain@Computer-2.local>
2024-10-16 12:30:39 -0400

    rm module-5 from cache

--- module-5/5.12/config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-import 'dotenv/config';
-
-const PORT = process.env.PORT || 3001;
-const MONGO_URI = process.env.MONGO_URI;
-const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
-
-export { PORT, MONGO_URI, MONGO_DB_NAME };
